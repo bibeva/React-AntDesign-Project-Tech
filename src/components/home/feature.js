94f006cc0d502d2c0adec7fa6eaff1bb2ce505f0
Sparsh101AI <sparshpatel6165@gmail.com>
2021-07-25 12:47:38 -0600

    ok

--- src/components/home/feature.js
+++ src/components/home/feature.js
@@ -17,7 +17,7 @@ function AppFeature() {
       <div className="container-fluid">
         <div className="titleHolder">
           <h2>Key Features and Benefits</h2>
-          <p>Obcaecati consequatur libero repudiandae, aperiam itaque laborum!</p>
+          <p>Obcssaecati consequatur libero repudiandae, aperiam itaque laborum!</p>
         </div>
         <Row gutter={[16, 16]}>
           <Col xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 8 }}>
